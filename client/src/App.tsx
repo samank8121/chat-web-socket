0905c7827bbeaee330d196ed2a2e22b21c56509e
saman <saman.k8121@gmail.com>
2025-01-07 15:02:56 +0300

    chore: change chat style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import io from 'socket.io-client';
 
@@ -13,11 +13,15 @@ function App() {
   const [messageSent, setMessageSent] = useState('');
   const [messages, setMessages] = useState<MessageType[]>([]);
   const [joinedRoom, setJoinedRoom] = useState('');
+  const lastMessageRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     socket.on('receive_message', (data) => {
       setMessages([...messages, { message: data.message, status: 'recieved' }]);
-    });   
+    });
+    if (lastMessageRef && lastMessageRef.current) {
+      lastMessageRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
   }, [messages]);
 
   const joinRoom = () => {
@@ -31,9 +35,10 @@ function App() {
   const sendMessage = () => {
     setMessages([...messages, { message: messageSent, status: 'sent' }]);
     socket.emit('send_message', { messageSent, room });
+    setMessageSent('');
   };
   return (
-    <div className='App'>
+    <div className='app '>
       <input
         placeholder='Room'
         onChange={(event) => {
@@ -43,6 +48,7 @@ function App() {
       <button onClick={joinRoom}> Join Room</button>
       <input
         placeholder='Message'
+        value={messageSent}
         onChange={(event) => {
           setMessageSent(event.target.value);
         }}
@@ -50,14 +56,14 @@ function App() {
       <button onClick={sendMessage}> Send Message</button>
       <h1> Messages:</h1>
       <div>{joinedRoom}</div>
-      <div>
+      <div className='message-container firefox-scroll'>
         {messages.map((m, index) => {
           return (
-            <>
+            <div ref={index === messages.length - 1 ? lastMessageRef : null}>
               <span key={index}>{`${m.status}: `}</span>
               <span key={index}>{m.message}</span>
-              <br/>
-            </>
+              <br />
+            </div>
           );
         })}
       </div>
